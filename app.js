0e5168ca3cbcca9ba4f6945bff6696a3375203f6
Jesus Santiago Hernandez Lopez <ingjesussantiago@gmail.com>
2023-09-02 17:51:23 -0600

    config de JSON, static, handlebars

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const PORT=8080
 
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static(__dirname + "/public"))
+
+
+
+app.engine("handlebars", handlebars.engine())
+app.set("views", __dirname + "/views")
+app.set("view engine", "handlebars")
+
+
 app.get("/",(req,res)=>{
     res.send(`desde es puerto ${PORT}`)
 })
